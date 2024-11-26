4cdd024b4c132b39d477c8db2c5e65ce23b02afe
Dennis Kibet <dennisrkibet@gmail.com>
2024-11-26 22:44:14 +0300

    created fetch middleware to handle data fetching and api intergration to the backend server

---
+++ src/lib/api.ts
@@ -0,0 +1,76 @@
+interface ErrorWithJson extends Error {
+  json: () => Promise<{ message?: string }>
+  status?: number
+}
+
+export const api = async (
+  method = 'GET',
+  slug = '',
+  data = {},
+  headers = {}
+) => {
+  try {
+    const config: RequestInit = {
+      method,
+      headers: { ...headers },
+      redirect: 'follow',
+      cache: 'no-store',
+      credentials: 'include',
+    }
+
+    if (method !== 'GET') {
+      config.body = JSON.stringify(data)
+    }
+
+    const url = `${process.env.BACKEND_URL}/api/${slug}`
+    const response = await fetch(url, config)
+    return response
+  } catch (error: unknown) {
+    console.error(error)
+
+    if (error instanceof Error) {
+      if (error.name === 'AbortError') {
+        return {
+          message: 'Request timed out',
+          status: 400,
+        }
+      } else if (error.message === 'Failed to fetch') {
+        return {
+          message: 'Connection timed out! Network Error',
+          status: 400,
+        }
+      }
+
+      let errorMessage = error.message || 'An unknown error occurred'
+      let status = 400
+
+      if (isErrorWithJson(error)) {
+        try {
+          const errorResponse = await error.json()
+          errorMessage = errorResponse.message || errorMessage
+          status = error.status || status
+        } catch (jsonError) {
+          console.error('Failed to parse error response', jsonError)
+        }
+      }
+
+      return {
+        message: errorMessage,
+        status,
+      }
+    }
+
+    return {
+      message: 'An unknown error occurred',
+      status: 400,
+    }
+  }
+}
+
+function isErrorWithJson(error: unknown): error is ErrorWithJson {
+  return (
+    error instanceof Error &&
+    'json' in error &&
+    typeof (error as ErrorWithJson).json === 'function'
+  )
+}
