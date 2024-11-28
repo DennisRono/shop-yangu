445a16ef50366c032089b8463a41ccbec128f4bc
Dennis Kibet <dennisrkibet@gmail.com>
2024-11-28 22:49:34 +0300

    implemented adding multiple products at once

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -7,17 +7,30 @@ import Product from '@/schemas/mongoose/ProductsSchema'
 export async function POST(request: NextRequest) {
   try {
     const data = await request.json()
-    console.log(data)
     const isConnected = await connectDB()
     if (!isConnected) {
       throw new CustomError('Failed to connect to database', 500)
     }
-    const newProduct = new Product(data)
-    await newProduct.save()
-    return NextResponse.json(
-      { message: 'Product added successfully!', product: newProduct },
-      { status: 201 }
-    )
+
+    if (Array.isArray(data)) {
+      const savedProducts = []
+      for (const product of data) {
+        const newProduct = new Product(product)
+        await newProduct.save()
+        savedProducts.push(newProduct)
+      }
+      return NextResponse.json(
+        { message: 'Products added successfully!', products: savedProducts },
+        { status: 201 }
+      )
+    } else {
+      const newProduct = new Product(data)
+      await newProduct.save()
+      return NextResponse.json(
+        { message: 'Product added successfully!', product: newProduct },
+        { status: 201 }
+      )
+    }
   } catch (error: any) {
     if (error.name === 'ValidationError') {
       return NextResponse.json({ message: error.message }, { status: 400 })
